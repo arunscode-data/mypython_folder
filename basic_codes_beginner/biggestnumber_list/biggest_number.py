5f41b638fedf5d3db181cf587c72d2bfb8e0d086
arunscode-data <71876247+arunscode-data@users.noreply.github.com>
2025-04-06 12:01:08 +0000

    Commit changes to find the biggest number in a list using a function

--- basic_codes_beginner/biggestnumber_list/biggest_number.py
+++ basic_codes_beginner/biggestnumber_list/biggest_number.py
@@ -1,8 +1,14 @@
 numbers = input("Enter numbers separated by comma: ").split(',')
-numbers = [int(num) for num in numbers]
-biggest_number = numbers[0]
-for number in numbers:
-    if number > biggest_number:
-        biggest_number = number
+# This code takes a list of numbers as input from the user, splits them by comma, and converts them to integers.
+
+def find_biggest_number(numbers):
+    numbers = [int(num) for num in numbers]
+    biggest_number = numbers[0]
+    for number in numbers:
+        if number > biggest_number:
+            biggest_number = number
+    return biggest_number
+
+biggest_number = find_biggest_number(numbers)
 print("The biggest number is:", biggest_number)
 # This code takes a list of numbers as input from the user, splits them by comma, converts them to integers, and finds the biggest number in the list.
